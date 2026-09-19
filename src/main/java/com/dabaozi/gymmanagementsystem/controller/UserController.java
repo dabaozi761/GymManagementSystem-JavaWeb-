943263597e0db968264a7d4806c7719085b5f783
dabaozi <15361420926@163.com>
2026-09-20 01:02:10 +0800

    test

--- src/main/java/com/dabaozi/gymmanagementsystem/controller/UserController.java
+++ src/main/java/com/dabaozi/gymmanagementsystem/controller/UserController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/hi")
-    public String Hi(){
+    public String Hi() {
         return "ok";
     }
+    a
 }
