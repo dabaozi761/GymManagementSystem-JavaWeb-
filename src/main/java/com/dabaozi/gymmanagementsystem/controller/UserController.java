4a12680db04a23e0a2f828eda1d3408e3fa35c65
dabaozi <15361420926@163.com>
2026-09-19 23:34:37 +0800

    init：创建用户controller，并测试是否可用

---
+++ src/main/java/com/dabaozi/gymmanagementsystem/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.dabaozi.gymmanagementsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class UserController {
+
+    @GetMapping("/hi")
+    public String Hi(){
+        return "ok";
+    }
+}
