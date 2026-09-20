13684c4eee5b041ae53e4e4e9c85b743999669fe
dabaozi <15361420926@163.com>
2026-09-20 23:06:06 +0800

    add:添加接口命名提示

--- src/main/java/com/dabaozi/gymmanagementsystem/controller/UserController.java
+++ src/main/java/com/dabaozi/gymmanagementsystem/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    //接口的统一命名格式请改为："api/gym-management-system/类名/v1/接口名"
+    //eg:api/gym-management-system/user/v1/login
     @GetMapping("/hi")
     public String Hi(){
         return "ok";
