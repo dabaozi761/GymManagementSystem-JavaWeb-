e0abd6fedc166234a55c9c5e2b41541f681d57d0
dabaozi <15361420926@163.com>
2026-09-22 00:47:21 +0800

    add:将注解启动

--- src/main/java/com/dabaozi/gymmanagementsystem/common/database/BaseDO.java
+++ src/main/java/com/dabaozi/gymmanagementsystem/common/database/BaseDO.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.annotation.TableLogic;
 import lombok.Data;
 
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 @Data
 public class BaseDO implements Serializable {
 
-//    @Serial
+    @Serial
     private static final long serialVersionUID = 1L;
 
     /**
