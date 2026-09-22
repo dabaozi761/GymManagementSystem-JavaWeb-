fd81360b3be655b54ab41c579af490ef71166047
dabaozi <15361420926@163.com>
2026-09-22 23:20:02 +0800

    fix

--- src/main/java/com/dabaozi/gymmanagementsystem/service/TrainingLogService.java
+++ src/main/java/com/dabaozi/gymmanagementsystem/service/TrainingLogService.java
@@ -1,12 +1,14 @@
 package com.dabaozi.gymmanagementsystem.service;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.dabaozi.gymmanagementsystem.pojo.dto.TrainingLogDTO;
 import com.dabaozi.gymmanagementsystem.pojo.dto.TrainingLogPageReqDTO;
+import com.dabaozi.gymmanagementsystem.pojo.entity.TrainingLog;
 import com.dabaozi.gymmanagementsystem.pojo.vo.TrainingLogPageRespDTO;
 
 
-public interface TrainingLogService {
+public interface TrainingLogService extends IService<TrainingLog> {
     void save(TrainingLogDTO trainingLogDTO);
 
     void deleteWithId(Long id);
